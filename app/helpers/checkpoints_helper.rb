7f29d185bf52d0eb47dc9375c498e123ac1eee62
Ahmed O <aomra015@gmail.com>
2013-08-21 17:09:48 -0400

    refactor progress bars and counter box into helper methods

--- app/helpers/checkpoints_helper.rb
+++ app/helpers/checkpoints_helper.rb
@@ -18,4 +18,23 @@ module CheckpointsHelper
     label
   end
 
+  def ratings_count_box(checkpoint)
+    ratings_count = checkpoint.ratings_count(@start_time, @end_time)
+    big_number = content_tag :div, ratings_count, class: 'count-number'
+    byline = content_tag :p, 'response'.pluralize(ratings_count)
+    big_number + byline
+  end
+
+  def progress_bars(checkpoint)
+    label_names = ["danger", "warning", "success"]
+    bars = []
+
+    label_names.each_with_index do |label, score|
+      bar = content_tag :div, get_rating_label(checkpoint, score), class: "progress-bar progress-bar-#{label}", style: "width: #{checkpoint.get_score(score, @start_time, @end_time)}%"
+      bars << bar
+    end
+
+    bars.sum
+  end
+
 end
