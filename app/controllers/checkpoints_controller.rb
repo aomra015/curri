43667c59d83ad1f4c9865b720aaa486802bfd5f5
Paula_Franzini <pjfranzini@hotmail.com>
2013-08-22 15:22:10 -0400

    added controller methods

--- app/controllers/checkpoints_controller.rb
+++ app/controllers/checkpoints_controller.rb
@@ -1,2 +1,41 @@
 class CheckpointsController < ApplicationController
+
+  def index
+    @checkpoints = Checkpoint.all
+  end
+
+  def show
+    @checkpoint = Checkpoint.find(params[:id])
+  end
+
+  def new
+    @checkpoint = Checkpoint.new
+  end
+
+  def create
+    @checkpoint = Checkpoint.new(checkpoint_params)
+    if @checkpoint.save
+      redirect_to checkpoint_path(@checkpoint), notice: "Checkpoint has been created"
+    else
+      redirect_to checkpoints_path
+    end
+  end
+
+  def edit
+    @checkpoint = Checkpoint.find(params[:id])
+  end
+
+  def update
+    if @checkpoint.update(checkpoint_params)
+      redirect_to checkpoint_path(@checkpoint), notice: "Checkpoint has been updated"
+    else
+      redirect_to checkpoints_path
+    end
+  end
+
+  private
+  def checkpoint_params
+    params.require(:checkpoint).permit(:expectation, :success_criteria, :track_id)
+  end
 end
+
--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -4,6 +4,35 @@ class TracksController < ApplicationController
     @tracks = Track.all
   end
 
+  def show
+    @track = Track.find(params[:id])
+  end
+
+  def new
+    @track = Track.new
+  end
+
+  def create
+    @track = Track.new(track_params)
+    if @track.save
+      redirect_to track_path(@track), notice: "Track has been created"
+    else
+      redirect_to tracks_path
+    end
+  end
+
+  def edit
+    @track = Track.find(params[:id])
+  end
+
+  def update
+    if @track.update(track_params)
+      redirect_to track_path(@track), notice: "Track has been updated"
+    else
+      redirect_to tracks_path
+    end
+  end
+
   def analytics
     @tracks = Track.all
     now = Time.zone.now
