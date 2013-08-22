22c29f9bd9be309a64964d48989b21b3526233f2
Ahmed O <aomra015@gmail.com>
2013-08-22 16:18:40 -0400

    add classroom controller tests and code

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,2 +1,43 @@
 class ClassroomsController < ApplicationController
+
+  before_action :get_classroom, only: [:show, :edit, :update]
+
+  def index
+    @classrooms = Classroom.all
+  end
+
+  def show
+  end
+
+  def new
+    @classroom = Classroom.new
+  end
+
+  def create
+    @classroom = Classroom.new(classroom_params)
+    if @classroom.save
+      redirect_to classroom_path(@classroom), notice: "classroom has been created"
+    else
+      redirect_to classrooms_path
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @classroom.update(classroom_params)
+      redirect_to classroom_path(@classroom), notice: "classroom has been updated"
+    else
+      redirect_to classrooms_path
+    end
+  end
+
+  private
+  def classroom_params
+    params.require(:classroom).permit(:name)
+  end
+  def get_classroom
+    @classroom = Classroom.find(params[:id])
+  end
 end
--- test/controllers/classrooms_controller_test.rb
+++ test/controllers/classrooms_controller_test.rb
@@ -1,7 +1,45 @@
 require 'test_helper'
 
 class ClassroomsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "get list of classrooms" do
+    get :index
+    assert assigns(:classrooms)
+    assert :success
+  end
+
+  test "show single classroom" do
+    get :show, id: classrooms(:one)
+    assert assigns(:classroom)
+    assert :success
+  end
+
+  test "get new classroom form" do
+    get :new
+    assert assigns(:classroom)
+    assert :success
+  end
+
+  test "create classroom" do
+    assert_difference 'Classroom.count' do
+      post :create, classroom: {name: "Test classroom"}
+    end
+
+    assert_redirected_to classroom_path(assigns(:classroom))
+  end
+
+  test "get edit classroom form" do
+    get :edit, id: classrooms(:one)
+    assert_equal classrooms(:one), assigns(:classroom)
+    assert :success
+  end
+
+  test "update classroom" do
+    classroom = Classroom.create(name: "classroom name")
+
+    patch :update, id: classroom, classroom: {name: "Changed name" }
+
+    classroom = Classroom.find(classroom.id)
+    assert_equal "Changed name", classroom.name
+    assert_redirected_to classroom_path(assigns(:classroom))
+  end
 end
