3580855ad0c91abfece5033db614f91e8d5e79ae
Ahmed O <aomra015@gmail.com>
2013-08-22 15:54:07 -0400

    add tests for tracks controller

--- test/controllers/tracks_controller_test.rb
+++ test/controllers/tracks_controller_test.rb
@@ -1,7 +1,45 @@
 require 'test_helper'
 
 class TracksControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "get list of tracks" do
+    get :index
+    assert assigns(:tracks)
+    assert :success
+  end
+
+  test "show single track" do
+    get :show, id: tracks(:one)
+    assert assigns(:track)
+    assert :success
+  end
+
+  test "get new track form" do
+    get :new
+    assert assigns(:track)
+    assert :success
+  end
+
+  test "create track" do
+    assert_difference 'Track.count' do
+      post :create, track: {name: "Test Track"}
+    end
+
+    assert_redirected_to track_path(assigns(:track))
+  end
+
+  test "get edit track form" do
+    get :edit, id: tracks(:one)
+    assert_equal tracks(:one), assigns(:track)
+    assert :success
+  end
+
+  test "update track" do
+    track = Track.create(name: "Track name")
+
+    patch :update, id: track, track: {name: "Changed name" }
+
+    track = Track.find(track.id)
+    assert_equal "Changed name", track.name
+    assert_redirected_to track_path(assigns(:track))
+  end
 end
