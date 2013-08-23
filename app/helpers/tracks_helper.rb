673ac09ee2d8fb6231940650dea7d38b86cbae03
Ahmed O <aomra015@gmail.com>
2013-08-22 21:19:11 -0400

    add model tests for checkpoint

--- app/helpers/tracks_helper.rb
+++ app/helpers/tracks_helper.rb
@@ -1,12 +1,12 @@
 module TracksHelper
 
-  def ratings_data
-    track = Track.first
-    track.checkpoints.map do |checkpoint|
-      {
-        checkpoint: checkpoint.expectation,
-        class_score: checkpoint.overall_score
-      }
-    end
-  end
+  # def ratings_data
+  #   track = Track.first
+  #   track.checkpoints.map do |checkpoint|
+  #     {
+  #       checkpoint: checkpoint.expectation,
+  #       class_score: checkpoint.overall_score
+  #     }
+  #   end
+  # end
 end
--- test/models/checkpoint_test.rb
+++ test/models/checkpoint_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class CheckpointTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    # fixture has two ratings each with a score of 1
+    @checkpoint = checkpoints(:one)
+    @start_time = @checkpoint.ratings.first.created_at - 1
+    @end_time = @checkpoint.ratings.last.created_at
+  end
+
+  test "ratings count" do
+    assert_equal 0, @checkpoint.ratings_count(@start_time, @end_time, 0)
+    assert_equal 0, @checkpoint.ratings_count(@start_time, @end_time, 2)
+    assert_equal 2, @checkpoint.ratings_count(@start_time, @end_time, 1)
+    assert_equal 2, @checkpoint.ratings_count(@start_time, @end_time)
+  end
+
+  test "get score" do
+    assert_equal 0, @checkpoint.get_score(0, @start_time, @end_time)
+    assert_equal 100, @checkpoint.get_score(1, @start_time, @end_time)
+    assert_equal 0, @checkpoint.get_score(2, @start_time, @end_time)
+  end
 end
