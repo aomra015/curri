6579440dc555f7fdd17394ecadca42628ef5503e
Ahmed O <aomra015@gmail.com>
2013-08-22 20:21:54 -0400

    progress bar partial used instead of a helper method

--- app/helpers/analytics_helper.rb
+++ app/helpers/analytics_helper.rb
@@ -1,2 +1,16 @@
 module AnalyticsHelper
+
+  def get_rating_label(checkpoint, score)
+    label = checkpoint.ratings_count(@start_time, @end_time, score)
+    label = "" if label == 0
+    label
+  end
+
+  def ratings_count_box(checkpoint)
+    ratings_count = checkpoint.ratings_count(@start_time, @end_time)
+    big_number = content_tag :div, ratings_count, class: 'count-number'
+    byline = content_tag :p, 'response'.pluralize(ratings_count)
+    big_number + byline
+  end
+
 end
