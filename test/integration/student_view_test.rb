7e4292b508f18114cac50fbccafb99ff7cf7d167
Ahmed Omran <aomra015@gmail.com>
2013-08-26 21:35:35 -0400

    added to-do to README

--- test/integration/student_view_test.rb
+++ test/integration/student_view_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class StudentViewTest < Capybara::Rails::TestCase
 
-  test "a teacher can rate checkpoints" do
+  test "a student can rate checkpoints" do
     skip
   end
 
