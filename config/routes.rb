d2e18c3fa018af2ed7fc708b25a98e789c9eb56d
Ahmed Omran <aomra015@gmail.com>
2013-08-27 16:07:58 -0400

    users can log out

--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,7 @@ Curry::Application.routes.draw do
 
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- test/integration/user_account_test.rb
+++ test/integration/user_account_test.rb
@@ -3,6 +3,10 @@ require 'test_helper'
 class UserAccountsTest < Capybara::Rails::TestCase
 
   test "a user can log out" do
-    skip
+    teacher = users(:ahmed)
+    login_as(teacher)
+    click_link 'Logout'
+
+    assert current_path == login_path, 'Did not redirect to login page'
   end
 end
