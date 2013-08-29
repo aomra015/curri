14b471f7297db1d4cca7722df9906ea12b026cbd
Nachiket Kumar <nk@nachiketkumar.com>
2013-08-29 13:39:14 -0400

    tracks index show expectations, track show styling

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -62,32 +62,4 @@ class Teacher < ActiveRecord::Base
       {expectation: "respond_to"}
       ])
   end
-
-  def add_default_classroom3
-    self.classrooms.create(name: "Sample Classroom3")
-
-    default_classroom = self.classrooms.find_by(name: "Sample Classroom3")
-
-    js_track = default_classroom.tracks.create(name: "jQuery UI Autocomplete")
-    api_track = default_classroom.tracks.create(name: "API Building")
-
-    js_track.checkpoints.create([
-      {expectation: "Installing a JS library"},
-      {expectation: "Selecting elements"},
-      {expectation: "Using jQuery UI widgets"},
-      {expectation: "Using an array source for autocomplete"},
-      {expectation: "Using a remote source for autocomplete"},
-      {expectation: "Using a remote function source for autocomplete"},
-      {expectation: "Sending Ajax requests"},
-      {expectation: "Parsing JSON results"}
-      ])
-
-    api_track.checkpoints.create([
-      {expectation: "What are formats?"},
-      {expectation: "What is JSON?"},
-      {expectation: "How to request a specific a format"},
-      {expectation: "respond_with"},
-      {expectation: "respond_to"}
-      ])
-  end
 end
