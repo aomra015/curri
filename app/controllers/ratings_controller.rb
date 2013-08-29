9a200cd22813d3e39850d31746b486d8505784e3
Nachiket Kumar <nk@nachiketkumar.com>
2013-08-28 21:27:19 -0400

    Make remote ratings update the page immediately.

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -3,7 +3,7 @@ class RatingsController < ApplicationController
   before_action :check_user_login
   before_action :get_nested_classroom
 
-  respond_to :json
+  respond_to :json, :js
 
   def create
     score = params[:value].to_i
@@ -11,12 +11,12 @@ class RatingsController < ApplicationController
     if !Rating::OPTIONS[score]
       head 422
     else
-      checkpoint = Checkpoint.find(params[:checkpoint_id])
+      @checkpoint = Checkpoint.find(params[:checkpoint_id])
 
-      session["checkpoint_#{checkpoint.id}"] = score
-      rating = checkpoint.ratings.create(score: score)
+      session["checkpoint_#{@checkpoint.id}"] = score
+      @rating = @checkpoint.ratings.create(score: score)
 
-      respond_with rating, location: nil
+      respond_with @rating, location: nil
     end
   end
 
