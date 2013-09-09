6ebc15f205e3b604f3d40726cbb6aa2325a1e9bd
Ahmed Omran <aomra015@gmail.com>
2013-09-09 16:27:14 -0400

    checkpoints can be updated individually

--- test/integration/tracks_test.rb
+++ test/integration/tracks_test.rb
@@ -6,23 +6,44 @@ class TracksTest < Capybara::Rails::TestCase
     teacher = users(:ahmed)
     login_as(teacher)
 
-    classroom = teacher.classrooms.first
+    classroom = classrooms(:one)
     click_link classroom.name
 
-    track = classroom.tracks.first
+    track = tracks(:one)
     click_link track.name
     assert_equal classroom_track_path(classroom, track), current_path
 
     click_link 'Add Checkpoints'
     assert_equal new_classroom_track_checkpoint_path(classroom, track), current_path
 
-    fill_in :checkpoint_expectation, with: 'Can do something'
-    fill_in :checkpoint_success_criteria, with: 'Did something'
+    fill_in :checkpoint_expectation, with: 'Can build a thing'
+    fill_in :checkpoint_success_criteria, with: 'something'
 
     assert_difference 'track.checkpoints.count' do
       click_button 'Create Checkpoint'
     end
+  end
 
+  test "a teacher can edit checkpoints" do
+    teacher = users(:ahmed)
+    login_as(teacher)
+
+    classroom = classrooms(:one)
+    click_link classroom.name
+    track = tracks(:one)
+    click_link track.name
+
+    checkpoint = checkpoints(:one)
+
+    within "#checkpoint#{checkpoint.id}" do
+        click_link 'Edit'
+    end
+
+    assert_equal edit_classroom_track_checkpoint_path(classroom, track, checkpoint), current_path
+
+    fill_in :checkpoint_expectation, with: 'Changed expectation'
+    fill_in :checkpoint_success_criteria, with: 'some criteria'
+    click_button 'Update Checkpoint'
   end
 
 end
