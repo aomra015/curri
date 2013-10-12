63d6167485d832d78a7cdde919ce2d782efaa2fe
Ahmed O <aomra015@gmail.com>
2013-10-12 16:20:48 -0400

    a teacher can edit classrooms

---
+++ test/integration/teacher_classrooms_test.rb
@@ -0,0 +1,35 @@
+require 'test_helper'
+
+class TeacherClassroomsTest < Capybara::Rails::TestCase
+
+  test "a teacher should see a list of their classrooms after login" do
+    teacher = users(:ahmed)
+    login_as(teacher)
+
+    classroom_name = teacher.classrooms.first.name
+    assert page.has_content?(classroom_name)
+
+    other_teachers_classroom = users(:paula).classrooms.first.name
+    assert !page.has_content?(other_teachers_classroom), "Another teacher's classroom is listed"
+
+    assert current_path == classrooms_path, "Not redirected to user's classrooms"
+  end
+
+  test "a teacher can edit classrooms" do
+    teacher = users(:ahmed)
+    login_as(teacher)
+
+    classroom = classrooms(:one)
+    click_link classroom.name
+
+    click_link 'manage-classroom'
+
+    assert_equal edit_classroom_path(classroom), current_path
+
+    fill_in :classroom_name, with: 'Changed classroom name'
+    click_button 'Update Classroom'
+
+    changed_classroom = Classroom.find(classrooms(:one).id)
+    assert_equal 'Changed classroom name', changed_classroom.name
+  end
+end
