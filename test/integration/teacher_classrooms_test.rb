dffd1e0901c0820d92888c422d9b72e9dfe5a729
Ahmed O <aomra015@gmail.com>
2013-10-12 16:57:06 -0400

    refactor tests with before blocks

--- test/integration/teacher_classrooms_test.rb
+++ test/integration/teacher_classrooms_test.rb
@@ -2,12 +2,14 @@ require 'test_helper'
 
 class TeacherClassroomsTest < Capybara::Rails::TestCase
 
-  test "a teacher should see a list of their classrooms after login" do
+  before do
     teacher = users(:ahmed)
     login_as(teacher)
+    @classroom = classrooms(:one)
+  end
 
-    classroom_name = teacher.classrooms.first.name
-    assert page.has_content?(classroom_name)
+  test "a teacher should see a list of their classrooms after login" do
+    assert page.has_content?(@classroom.name)
 
     other_teachers_classroom = users(:paula).classrooms.first.name
     assert !page.has_content?(other_teachers_classroom), "Another teacher's classroom is listed"
@@ -16,29 +18,21 @@ class TeacherClassroomsTest < Capybara::Rails::TestCase
   end
 
   test "a teacher can edit classrooms" do
-    teacher = users(:ahmed)
-    login_as(teacher)
-
-    classroom = classrooms(:one)
-    click_link classroom.name
+    click_link @classroom.name
 
     click_link 'manage-classroom'
 
-    assert_equal edit_classroom_path(classroom), current_path
+    assert_equal edit_classroom_path(@classroom), current_path
 
     fill_in :classroom_name, with: 'Changed classroom name'
     click_button 'Update Classroom'
 
-    changed_classroom = Classroom.find(classrooms(:one).id)
+    changed_classroom = Classroom.find(@classroom.id)
     assert_equal 'Changed classroom name', changed_classroom.name
   end
 
   test "a teacher can delete classrooms" do
-    teacher = users(:ahmed)
-    login_as(teacher)
-
-    classroom = classrooms(:one)
-    click_link classroom.name
+    click_link @classroom.name
 
     click_link 'manage-classroom'
 
--- test/integration/teacher_invitations_test.rb
+++ test/integration/teacher_invitations_test.rb
@@ -2,13 +2,15 @@ require 'test_helper'
 
 class TeacherInvitationsTest < Capybara::Rails::TestCase
 
-  test "teacher can invite students" do
-    teacher = users(:ahmed)
-    student_email = 'mystudent@gmail.com'
-    login_as(teacher)
+  before do
+    @teacher = users(:ahmed)
+    @student_email = users(:student).email
+    login_as(@teacher)
+  end
 
+  test "teacher can invite students" do
     assert_difference 'Invitation.count' do
-      invite_students(teacher, student_email)
+      invite_students(@teacher, @student_email)
     end
 
     assert_equal classroom_tracks_path(@classroom), current_path
@@ -16,24 +18,18 @@ class TeacherInvitationsTest < Capybara::Rails::TestCase
 
     last_email = ActionMailer::Base.deliveries.last
 
-    assert_equal [student_email], last_email.to
+    assert_equal [@student_email], last_email.to
   end
 
   test "teacher should see list of invited students" do
-    teacher = users(:ahmed)
-    login_as(teacher)
-    student_email = 'mystudent@gmail.com'
-    invite_students(teacher, student_email)
+    invite_students(@teacher, @student_email)
 
     click_link 'manage-students'
-    assert page.has_content?(student_email), 'Email of invited student not listed'
+    assert page.has_content?(@student_email), 'Email of invited student not listed'
   end
 
   test "teacher can cancel invitations" do
-    teacher = users(:ahmed)
-    login_as(teacher)
-    student_email = 'mystudent@gmail.com'
-    invite_students(teacher, student_email)
+    invite_students(@teacher, @student_email)
 
     invitation = Invitation.last
     click_link 'manage-students'
