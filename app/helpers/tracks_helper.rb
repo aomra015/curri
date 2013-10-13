aab455798f9d284a4cfaa57c18f10114ce53c06e
Ahmed O <aomra015@gmail.com>
2013-10-13 14:28:49 -0400

    Once a user has set time data for a track, this time data should be visible on the track page not just in the edit page.

--- app/helpers/tracks_helper.rb
+++ app/helpers/tracks_helper.rb
@@ -13,4 +13,14 @@ module TracksHelper
   def nice_time_format(track, method)
     track.send(method).to_s(:time) if track.send(method)
   end
+
+  def display_start_end_times(track)
+    if track.phasing?
+      start_string = track.start_date.to_formatted_s(:rfc822) + " "
+      start_string += track.start_time.strftime("%l:%M%P")
+      end_string = track.end_date.to_formatted_s(:rfc822) + " "
+      end_string += track.end_time.strftime("%l:%M%P")
+      "From #{start_string} to #{end_string}"
+    end
+  end
 end
