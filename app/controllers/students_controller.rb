03a672c13b8dc28de7e4fe43a5f46854799b16b8
Paula_Franzini <pjfranzini@hotmail.com>
2013-11-21 16:33:32 -0500

    url name improved; redirect :back made more error proof; studentsControllerTest added

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,7 +4,7 @@ class StudentsController < ApplicationController
 
   def update_status
     @current_user.classrole.toggle(:help).save
-    redirect_to :back, notice: "Help status toggled."
+    redirect_to request.env['HTTP_REFERER'] ? :back : classrooms_path, notice: "Help status toggled."
   end
 
 end
