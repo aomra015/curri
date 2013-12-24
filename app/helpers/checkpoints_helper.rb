cc4cc21feb9741aa9db23e9c67f0045ac26d9e4f
Ahmed Omran <aomra015@gmail.com>
2013-12-24 10:32:41 -0500

    remove unused helper methods

--- app/helpers/checkpoints_helper.rb
+++ app/helpers/checkpoints_helper.rb
@@ -1,13 +1,5 @@
 module CheckpointsHelper
 
-  def checkpoint_select_tag_name(checkpoint)
-    "checkpoints[checkpoint_#{checkpoint.id}][:score]]"
-  end
-
-  def checkpoint_hidden_tag_name(checkpoint)
-    "checkpoints[checkpoint_#{checkpoint.id}][:id]]"
-  end
-
   def checkpoint_class_name(checkpoint)
     "checkpoint_" + checkpoint.latest_student_score(current_user.classrole)
   end
