d0fe18c5a93a102c34dc0e4a751d22955f686b82
Ahmed Omran <aomra015@gmail.com>
2013-12-30 23:01:24 -0500

    Unit tests added for student rating feature

--- test/controllers/ratings_controller_test.rb
+++ test/controllers/ratings_controller_test.rb
@@ -4,11 +4,10 @@ class RatingsControllerTest < ActionController::TestCase
 
   before do
     session[:user_id] = users(:student).id
+    PrivatePub.stubs(:publish_to)
   end
 
   test "should create ratings with html request" do
-    PrivatePub.expects(:publish_to).once
-
     assert_difference 'Rating.count' do
       post :create, classroom_id: classrooms(:one), track_id: tracks(:one), checkpoint_id: checkpoints(:one).id, value: 0
     end
@@ -16,8 +15,6 @@ class RatingsControllerTest < ActionController::TestCase
   end
 
   test "should create ratings with json request" do
-    PrivatePub.expects(:publish_to).once
-
     assert_difference 'Rating.count' do
       post :create, format: 'json', classroom_id: classrooms(:one), track_id: tracks(:one), checkpoint_id: checkpoints(:one).id, value: 0
     end
@@ -25,6 +22,16 @@ class RatingsControllerTest < ActionController::TestCase
     response = JSON.parse(@response.body)
     assert_equal 0, response["current_score"]
     assert_equal checkpoints(:one).id, response["checkpoint_id"]
+  end
+
+  test "rating should be associated with logged-in student" do
+    post :create, classroom_id: classrooms(:one), track_id: tracks(:one), checkpoint_id: checkpoints(:one).id, value: 0
+    assert_equal users(:student).classrole, Rating.last.student
+  end
+
+  test "should publish data to push server" do
+    PrivatePub.expects(:publish_to).once
+    post :create, classroom_id: classrooms(:one), track_id: tracks(:one), checkpoint_id: checkpoints(:one).id, value: 0
   end
 
   test "only students should be able to rate checkpoints" do
---
+++ test/features/student_ratings_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class StudentRatingsTest < Capybara::Rails::TestCase
+
+  test "a student can rate checkpoints" do
+    PrivatePub.stubs(:publish_to)
+
+    student = users(:student)
+    login_as(student)
+
+    click_link classrooms(:one).name
+    click_link tracks(:one).name
+
+    checkpoint = checkpoints(:one)
+    assert page.has_content?(checkpoint.expectation)
+
+    within "#checkpoint#{checkpoint.id}" do
+      click_button 'Totally Understand'
+    end
+
+    assert find("#checkpoint#{checkpoint.id}").has_css?('.checkpoint_2')
+  end
+
+end
