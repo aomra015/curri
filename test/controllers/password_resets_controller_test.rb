9196a93abf701c534f6cb50dc46592aaed8e52ae
Ahmed Omran <aomra015@gmail.com>
2014-01-02 17:32:09 -0500

    password reset more tests added

--- test/controllers/password_resets_controller_test.rb
+++ test/controllers/password_resets_controller_test.rb
@@ -1,6 +1,12 @@
 require 'test_helper'
 
 class PasswordResetsControllerTest < ActionController::TestCase
+
+  before do
+    users(:paula).send_password_reset
+    @token = users(:paula).reload.password_reset_token
+  end
+
   test "should get new" do
     get :new
     assert_response :success
@@ -11,6 +17,36 @@ class PasswordResetsControllerTest < ActionController::TestCase
     assert_equal users(:paula), assigns(:user)
     assert_respond_to(assigns(:user), :send_password_reset)
     assert_redirected_to login_path
+  end
+
+  test "should show password edit form" do
+    get :edit, id: @token
+
+    assert_equal users(:paula), assigns(:user)
+    assert :success
+  end
+
+  test "should update password with valid token" do
+    post :update, id: @token, user: { password: 'newpassword', password_confirmation: 'newpassword'}
+
+    assert users(:paula).reload.authenticate('newpassword')
+    assert_redirected_to root_url
+  end
+
+  test "should not update password with password mismatch" do
+    post :update, id: @token, user: { password: 'newpassword', password_confirmation: 'newpassword22'}
+
+    assert_template :edit
+  end
+
+  test "should give error with old token" do
+    users(:paula).password_reset_sent_at = Time.zone.now - 5.hours
+    users(:paula).save
+
+    post :update, id: @token, user: { password: 'newpassword', password_confirmation: 'newpassword'}
+
+    assert_redirected_to new_password_reset_path
+    assert_equal "Permission to change password has expired.", flash[:alert]
   end
 
 end
