125a5ef83829a11396c2831ca6f09f51e451ed86
Ahmed Omran <aomra015@gmail.com>
2014-01-02 18:10:40 -0500

    classroom tests fixed up a bit

--- test/models/classroom_test.rb
+++ test/models/classroom_test.rb
@@ -14,14 +14,22 @@ class ClassroomTest < ActiveSupport::TestCase
     assert classroom.errors[:name].any?
   end
 
-  test "get requesters" do
-    date_in_past = Time.zone.now - 1
-    @invitation1.toggle(:help).save
-    @invitation1.updated_at = date_in_past
-    @invitation1.save
-    @invitation2.toggle(:help).save
-    assert_equal @invitation1,@classroom.requesters.first
-    assert_equal @invitation2,@classroom.requesters.last
+  test "requesters list" do
+    @invitation1.toggle!(:help)
+    @invitation2.toggle!(:help)
+
     assert_equal 2, @classroom.requesters.size
+  end
+
+  test "requesters in ASC order by updated_at" do
+    # First Request
+    @invitation1.toggle!(:help)
+    # Newer Request
+    @invitation2.toggle(:help)
+    @invitation2.updated_at = Time.zone.now - 1
+    @invitation2.save
+
+    assert_equal @invitation2,@classroom.requesters.first
+    assert_equal @invitation1,@classroom.requesters.last
   end
 end
