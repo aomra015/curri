2d3431bb0983e7f6a8ad78985a23ccab8dff9514
Ahmed Omran <aomra015@gmail.com>
2014-01-03 14:27:00 -0500

    link_to_selected modified

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,21 @@
 module ApplicationHelper
 
   def link_to_selected(*arg, &block)
-    if current_page?(arg[0])
+    if current_page?(arg[0]) || at_track?(arg[0]) || at_classroom?(arg[0])
       options = arg.extract_options!
       options[:class] += ' selected'
       arg << options
     end
 
     link_to(*arg, &block)
+  end
+
+  def at_classroom?(path)
+    path.match(/classrooms$/) && (params[:controller] == 'classrooms' || params[:controller] == 'invitations')
+  end
+
+  def at_track?(path)
+    path.match(/tracks$/) && (params[:controller] == 'tracks' || params[:controller] == 'checkpoints')
   end
 
 end
