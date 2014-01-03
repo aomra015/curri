d366d45ffc62c2878e8e8db0b224a351a3f5165d
Ahmed Omran <aomra015@gmail.com>
2014-01-02 19:34:47 -0500

    helper creater for select link class

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def link_to_selected(*arg, &block)
+    if current_page?(arg[0])
+      options = arg.extract_options!
+      options[:class] += ' selected'
+      arg << options
+    end
+
+    link_to(*arg, &block)
+  end
+
 end
