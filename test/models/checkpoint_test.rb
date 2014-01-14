e49eab7fe03537d3c19aed2c9f5242a4b75f30e2
Ahmed Omran <aomra015@gmail.com>
2014-01-14 13:28:31 -0500

    remove before block for checkpoint test

--- test/models/checkpoint_test.rb
+++ test/models/checkpoint_test.rb
@@ -2,14 +2,6 @@ require 'test_helper'
 
 class CheckpointTest < ActiveSupport::TestCase
 
-  before do
-    # stubbing time
-    date_in_future = Time.zone.now + 5.days
-    Time.zone.expects(:now).returns(date_in_future)
-
-    @phase = Phase.new(tracks(:one),"Realtime")
-  end
-
   test "validate presence of both attributes" do
     checkpoint = Checkpoint.new(expectation: 'something')
     checkpoint.valid?
@@ -29,9 +21,15 @@ class CheckpointTest < ActiveSupport::TestCase
   end
 
   test "hasnt voted list for class with two students" do
+    # stubbing time
+    date_in_future = Time.zone.now + 5.days
+    Time.zone.expects(:now).returns(date_in_future)
+
+    phase = Phase.new(tracks(:one),"Realtime")
     classroom = classrooms(:one)
-    assert_equal ["all"], checkpoints(:noratings).hasnt_voted(@phase, classroom)
-    assert_equal [], checkpoints(:one).hasnt_voted(@phase, classroom)
-    assert_equal ['student2@school.com'], checkpoints(:onerating).hasnt_voted(@phase, classroom)
+
+    assert_equal ["all"], checkpoints(:noratings).hasnt_voted(phase, classroom)
+    assert_equal [], checkpoints(:one).hasnt_voted(phase, classroom)
+    assert_equal ['student2@school.com'], checkpoints(:onerating).hasnt_voted(phase, classroom)
   end
 end
