23a8ff05ccb1374e85a3ec1c5d17139c48f74f79
Ahmed Omran <aomra015@gmail.com>
2014-01-17 11:00:15 -0500

    refactor unpublished tracks count to a helper

--- app/helpers/classrooms_helper.rb
+++ app/helpers/classrooms_helper.rb
@@ -7,4 +7,12 @@ module ClassroomsHelper
       content_tag :div, '0 invitations pending', class: 'no-pending-inv h5'
     end
   end
+
+  def unpublished_tracks(unpublished, published)
+    if unpublished != 0
+      content_tag :div, "#{unpublished} #{'track'.pluralize(unpublished)} unpublished", class: 'unpublished-tracks h5'
+    elsif published != 0
+      content_tag :div, '0 tracks unpublished', class: 'all-published h5'
+    end
+  end
 end
