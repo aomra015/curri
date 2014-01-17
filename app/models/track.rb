5baa2ba62f875c2c16b821fa916f9cfa3a437ef4
Ahmed Omran <aomra015@gmail.com>
2014-01-17 18:12:44 -0500

    move student_ratings query from student model to track model.

--- app/models/track.rb
+++ app/models/track.rb
@@ -14,4 +14,8 @@ class Track < ActiveRecord::Base
   def ratings
     Rating.where({ checkpoint_id: checkpoints.pluck(:id) }).select("DISTINCT ON (student_id, checkpoint_id) * ").order("student_id, checkpoint_id, created_at DESC")
   end
+
+  def student_ratings(student)
+    student.ratings.where({ checkpoint_id: checkpoints.pluck(:id) }).select("DISTINCT ON (checkpoint_id) * ").order("checkpoint_id, created_at DESC")
+  end
 end
