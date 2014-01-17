886c49e237aa6b00be1003d8ca2927bf3db2b578
Ahmed Omran <aomra015@gmail.com>
2014-01-17 10:51:25 -0500

    refactor pending invitations notice to a helper

--- app/helpers/classrooms_helper.rb
+++ app/helpers/classrooms_helper.rb
@@ -1,2 +1,10 @@
 module ClassroomsHelper
+
+  def pending_invitations(pending, accepted)
+    if pending != 0
+      content_tag :div, "#{pending} #{'invitation'.pluralize(pending)} pending", class: 'pending-inv h5'
+    elsif accepted != 0
+      content_tag :div, '0 invitations pending', class: 'no-pending-inv h5'
+    end
+  end
 end
