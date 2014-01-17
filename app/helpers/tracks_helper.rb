f59b1013352845eefdd39aef5788d3ff5fa45fd6
Ahmed Omran <aomra015@gmail.com>
2014-01-17 16:48:56 -0500

    start/end times fixed on tracks index page

--- app/helpers/tracks_helper.rb
+++ app/helpers/tracks_helper.rb
@@ -16,8 +16,19 @@ module TracksHelper
     end
   end
 
-  # def display_start_time(track)
-  #   start_string = track.start_time.strftime("%l:%M%P, %e %b %y")
-  #   "Track start: #{start_string}"
-  # end
+  def display_start_time(track)
+    if track.start_time
+      "Track start: #{track.start_time.strftime("%l:%M%P, %e %b %y")}"
+    else
+      "no start time defined"
+    end
+  end
+
+  def display_end_time(track)
+    if track.end_time
+      "Track end: #{track.end_time.strftime("%l:%M%P, %e %b %y")}"
+    else
+      "no end time defined"
+    end
+  end
 end
