59681ac50d988178b8ee09734bf0fdcf02007f70
Paula Franzini <pjfranzini@hotmail.com>
2014-01-18 16:35:57 -0500

    added pahse phase property to View analytics page event, so that teacher use of phasing can be tracked

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -6,7 +6,7 @@ class AnalyticsController < ApplicationController
   def show
     @track = @classroom.tracks.find(params[:track_id])
     @phase = Phase.new(@track, params[:phase_state] || "Realtime")
-    flash.now[:track] = { event_name: "View analytics page", properties: {classroom_id: @classroom.id, track_id: @track.id } }
+    flash.now[:track] = { event_name: "View analytics page", properties: {classroom_id: @classroom.id, track_id: @track.id, phase_state: @phase.state } }
   end
 
 end
