b20c93eced6f79d8579da9254c06f1e13ed3f3b9
Ahmed O <aomra015@gmail.com>
2014-01-27 13:26:49 -0500

    refactor ratings controller

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -11,19 +11,15 @@ class RatingsController < ApplicationController
     if !Rating::OPTIONS[score]
       head 422
     else
-      @rating = @checkpoint.ratings.new(score: score)
-      @rating.student = current_user.classrole
-      @rating.save
-
-      ratings = @checkpoint.ratings.distinct_by_student.to_json(only: :score)
-      Pusher.trigger("track#{@track.id}-ratings", 'rating', { checkpoint: @checkpoint.id, ratings: ratings, totalCount: @classroom.students.size })
+      rating = @checkpoint.ratings.create(score: score, student: current_user.classrole)
+      push_to_channel
 
       respond_to do |format|
         format.json {
-          render json: { classroom_id: @classroom.id, track_id: @track.id, checkpoint_id: @checkpoint.id, current_score: @rating.score, partial: render_to_string(partial: "face#{@rating.score}.html", formats: :html) }
+          render json: { classroom_id: @classroom.id, track_id: @track.id, checkpoint_id: @checkpoint.id, current_score: rating.score, partial: render_to_string(partial: "face#{rating.score}.html", formats: :html) }
         }
         format.html {
-          redirect_to classroom_track_url(@classroom, @track)
+          redirect_to classroom_track_url(@classroom, @track), notice: 'Checkpoint rating recorded.'
         }
       end
 
@@ -33,6 +29,11 @@ class RatingsController < ApplicationController
   private
   def authorize_student
     redirect_to classroom_track_path(params[:classroom_id], params[:track_id]), alert: "Only students can rate" if current_user.classrole_type != 'Student'
+  end
+
+  def push_to_channel
+    ratings = @checkpoint.ratings.distinct_by_student.to_json(only: :score)
+    Pusher.trigger("track#{@track.id}-ratings", 'rating', { checkpoint: @checkpoint.id, ratings: ratings, totalCount: @classroom.students.size })
   end
 
 end
