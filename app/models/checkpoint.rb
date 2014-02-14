f9be470b8c6554d77f953ca31bf00303028a6690
Ahmed Omran <aomra015@gmail.com>
2014-02-14 10:29:34 -0500

    fixes #129 -- hasnt_voted changed to 0.75 and adjusted max length of checkpoint and classrooms

--- app/models/checkpoint.rb
+++ app/models/checkpoint.rb
@@ -5,7 +5,7 @@ class Checkpoint < ActiveRecord::Base
 
   validates :expectation, :success_criteria, presence: true
   validates_length_of :expectation, :maximum => 50
-  validates_length_of :success_criteria, :maximum => 200
+  validates_length_of :success_criteria, :maximum => 300
 
   def latest_student_score(student)
     ratings.where(student_id: student.id).last.try(:score)
@@ -16,7 +16,7 @@ class Checkpoint < ActiveRecord::Base
     numb_ratings = scoped_ratings.length
     numb_students = classroom.students.count
 
-    if (numb_ratings >= (numb_students*0.70).floor) && (numb_ratings < numb_students)
+    if (numb_ratings >= (numb_students*0.75).floor) && (numb_ratings < numb_students)
       student_list = classroom.students.pluck(:id)
       scoped_ratings.includes(:student).each do |rating|
         student_list.delete(rating.student.id)
